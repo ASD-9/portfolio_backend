4cf3d5a7975fe458f3a2642c1c3c7a7a5de03228
ASD-9 <ryan.chavatte@gmail.com>
2025-04-11 00:31:18 +0200

    initial commit

---
+++ src/server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const app = express();
+
+app.disable("x-powered-by");
+
+app.use(cors());
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
