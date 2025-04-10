887ff18ed4b473344ef0b995fd7c415d228928c0
ASD-9 <ryan.chavatte@gmail.com>
2025-04-11 00:50:02 +0200

    add connection to database

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 
 dotenv.config();
@@ -12,6 +13,13 @@ app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+const mongo_uri = process.env.MONGO_URI || "mongodb://root:root@localhost:27017";
+
+mongoose
+  .connect(mongo_uri)
+  .then(() => console.log("Connected to database"))
+  .catch((error) => console.log(error));
 
 const PORT = process.env.PORT || 3000;
 
