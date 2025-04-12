f6a1e34678cee9f1b3407fc53de7dbe93eff27dd
ASD-9 <ryan.chavatte@gmail.com>
2025-04-12 15:42:02 +0200

    edit return messages

--- src/authController.js
+++ src/authController.js
@@ -7,19 +7,19 @@ const login = async (req, res) => {
 
     const user = await User.findOne({ username });
     if (!user) {
-      return res.status(401).json({ message: "Nom d'utilisateur ou mot de passe incorrect" });
+      return res.status(401).json({ message: "❌ Nom d'utilisateur ou mot de passe incorrect" });
     }
     const passwordMatch = await bcrypt.compare(password, user.password);
     if (!passwordMatch) {
-      return res.status(401).json({ message: "Nom d'utilisateur ou mot de passe incorrect" });
+      return res.status(401).json({ message: "❌ Nom d'utilisateur ou mot de passe incorrect" });
     }
 
     const userInfo = user.toObject();
     delete userInfo.password;
 
-    res.status(200).json({ message: "Connexion réussie", data: userInfo });
+    res.status(200).json({ message: "✅ Connexion réussie", data: userInfo });
   } catch (error) {
-    res.status(500).json({ message: "Erreur serveur", error: error.message });
+    res.status(500).json({ message: "💥 Problème interne au serveur, réessayez plus tard.", error: error.message });
   }
 }
 
